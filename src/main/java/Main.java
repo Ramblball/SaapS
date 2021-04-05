f54148b4658dcd832a97ea541441761a11b7212d
Maxim <lostrandg@gmail.com>
2021-04-06 01:34:29 +0500

    feature(app): добавление логгирования

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,13 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import servers.chat.Chat;
 
 public class Main {
+    private static final Logger logger = LogManager.getLogger(Main.class);
+
     public static void main(String[] args) {
         new Chat().start();
+
+        logger.debug("Server started");
     }
 }
