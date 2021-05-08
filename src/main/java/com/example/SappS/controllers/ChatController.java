dc90ecfe8b6ead3f3b2e89ab49c939c7d1183451
Maxim <lostrandg@gmail.com>
2021-05-08 22:27:48 +0500

    feature(controller): добавление обработчика получения друга

--- src/main/java/com/example/SappS/controllers/ChatController.java
+++ src/main/java/com/example/SappS/controllers/ChatController.java
@@ -1,15 +1,24 @@
 package com.example.SappS.controllers;
 
+import com.example.SappS.controllers.payload.UserResponse;
+import com.example.SappS.database.exceptions.NotFoundException;
 import com.example.SappS.database.models.Message;
+import com.example.SappS.database.models.User;
 import com.example.SappS.database.services.MessageService;
 import com.example.SappS.database.services.RoomService;
+import com.example.SappS.database.services.UserService;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Optional;
 
 @Controller
 @AllArgsConstructor
@@ -18,6 +27,7 @@ public class ChatController {
 
     SimpMessagingTemplate messagingTemplate;
     MessageService messageService;
+    UserService userService;
     RoomService roomService;
 
     @MessageMapping("/chat")
@@ -28,5 +38,16 @@ public class ChatController {
 
         messagingTemplate.convertAndSendToUser(
                 message.getReceiverId(), "/queue/messages", saved);
+    }
+
+    @GetMapping(value = "/friend/{name}")
+    public ResponseEntity<?> getFriendUser(@PathVariable String name) {
+        Optional<User> friend = userService.findByName(name);
+        if (friend.isEmpty()) {
+            throw new NotFoundException("User " + name + " not found");
+        }
+        User user = friend.get();
+        UserResponse response = new UserResponse(user.getId(), user.getName(), user.getAge(), user.getCity());
+        return ResponseEntity.ok(response);
     }
 }
--- src/main/java/com/example/SappS/controllers/UserController.java
+++ src/main/java/com/example/SappS/controllers/UserController.java
@@ -6,10 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
